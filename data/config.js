e6867b5f6590c2f3f55ef477ea6ce54cb2b19ef7
Ana Franco <135935235+ANAFRAN82@users.noreply.github.com>
2025-06-18 15:49:12 -0600

    Update config.js

--- data/config.js
+++ data/config.js
@@ -1,21 +1,17 @@
-// Dependencia de mongoose 
 const mongoose = require("mongoose");
-// Dependencia para el archivo .env
 require("dotenv").config();
 
-// Extraer en una url lo que contiene el .env
-const URL = process.env.URL;
+// Usar MONGODB_URL que Railway proporciona automáticamente
+const URL = process.env.MONGODB_URL || process.env.URL;
 
-// Conexion a la base de datos 
 const ConnectDB = async () => {
-try{
+  try {
     await mongoose.connect(URL);
-    // Mensaje de que la base de datos funciona correctamente
     console.log("Database running");
-}catch(error){
-    // Mensaje de error si la conexion falla
-    console.log("Cant connecto to database "+ error);
-}  
-}
-// Exportacion del modulo
-module.exports = {ConnectDB};
+  } catch (error) {
+    console.log("Can't connect to database " + error);
+    process.exit(1); // Salir si no se puede conectar
+  }
+};
+
+module.exports = { ConnectDB };
